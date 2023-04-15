34174ecbb64d0d7c6aff15a8b26d609481f2abca
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-15 09:36:17 -0500

    Test

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ def check(ID):
     cursor.execute(Query)
     for item in cursor:
         print(item)
-def add(ID,amount):
-    i = 4;
-    i = i+1
 def create(name,balance,cred):
     addData = ("INSERT INTO accounts(accountName,accountBalance,credit) VALUES(" + name + "," + balance + "," + cred + ")")
     cursor.execute(addData)
+    connection.commit
 def show():
     testQuery = ("SELECT * FROM accounts")
     cursor.execute(testQuery)
-
+create('John','6900','450')
 show()
+connection.commit()
 cursor.close()
 connection.close()
