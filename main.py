5df309151615228a69e5095251415c41608a64c4
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-14 20:51:57 -0500

    Added check balance function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import mysql.connector
 connection = mysql.connector.connect(user = 'root',database='bankingproject',password='TheFinisher518!')
 cursor = connection.cursor()
-addData =("INSERT INTO accounts(accountName,accountBalance,credit) VALUES('Joe',5432.81,740)")
-cursor.execute(addData)
-testQuery = ("SELECT * FROM accounts")
-cursor.execute(testQuery)
-for item in cursor:
-    print(item)
+def check(ID):
+    Query = ("SELECT accountBalance FROM accounts Where accountID = ID")
+    cursor.execute(Query)
+    for item in cursor:
+        print(item)
+check(6);
 connection.commit()
 cursor.close()
 connection.close()
