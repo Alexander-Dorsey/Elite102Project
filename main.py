3f092951305b1d74d4e0b8c8eeb961482b6f5d37
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-14 20:30:25 -0500

    Testing Continues

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import mysql.connector
 connection = mysql.connector.connect(user = 'root',database='bankingproject',password='TheFinisher518!')
 cursor = connection.cursor()
-cursor.execute("INSERT INTO accounts(accountName,AccountBalance,AccountCredit) VALUES('Joe','5432.81','740')")
+addData =("INSERT INTO accounts(accountName,AccountBalance,AccountCredit) VALUES('Joe','5432.81','740')")
+cursor.execute(addData)
 testQuery = "SELECT * FROM accounts"
 cursor.execute(testQuery)
 for item in cursor:
     print(item)
+connection.commit()
 cursor.close()
 connection.close()
