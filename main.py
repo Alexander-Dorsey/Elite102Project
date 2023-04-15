d8e77ddbdd40c538f1885b43f2f378699411363a
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-14 20:36:09 -0500

    Test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import mysql.connector
 connection = mysql.connector.connect(user = 'root',database='bankingproject',password='TheFinisher518!')
 cursor = connection.cursor()
-addData =("INSERT INTO accounts(accountName,accountBalance,credit VALUES('Joe','5432.81','740')")
+addData =("INSERT INTO accounts(accountName,accountBalance,credit VALUES('Joe',5432.81,740)")
 cursor.execute(addData)
 connection.commit()
 cursor.close()
