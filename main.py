7af564ceecb8c4078b54c665787b766baac9593a
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-15 08:57:23 -0500

    Check func attempt 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mysql.connector
 connection = mysql.connector.connect(user = 'root',database='bankingproject',password='TheFinisher518!')
 cursor = connection.cursor()
 def check(ID):
-    Query = ("SELECT accountBalance FROM accounts Where accountID = ID")
+    Query = ("SELECT accountBalance FROM accounts Where accountID = " + ID + "")
     cursor.execute(Query)
     for item in cursor:
         print(item)
