c0f09ddbcaef729c1676cee01d2f4a25e7e01023
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-14 20:25:39 -0500

    Test Continues

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import mysql.connector
 connection = mysql.connector.connect(user = 'root',database='bankingproject',password='TheFinisher518!')
-cursor = connection.cursor
-test = "SELECT * FROM accounts"
-cursor.execute(test)
+cursor = connection.cursor()
+testQuery = "SELECT * FROM accounts"
+cursor.execute(testQuery)
 for item in cursor:
     print(item)
 cursor.close()
