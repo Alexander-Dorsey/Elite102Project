76957cacf5b36f160c342295c6a82df45040041b
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-14 20:39:29 -0500

    Final Test

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ connection = mysql.connector.connect(user = 'root',database='bankingproject',pas
 cursor = connection.cursor()
 addData =("INSERT INTO accounts(accountName,accountBalance,credit) VALUES('Joe',5432.81,740)")
 cursor.execute(addData)
+testQuery = ("SELECT * FROM account")
+cursor.execute(testQuery)
+for item in cursor:
+    print(item)
 connection.commit()
 cursor.close()
 connection.close()
