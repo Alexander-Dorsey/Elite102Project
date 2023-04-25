fda6a3173234842511513f3e2f256809e9b8c83d
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-25 18:09:13 -0500

    More UI

---
+++ new_student_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
+
+class NewStudentForm(FlaskForm):
+    firstname = StringField("First Name", validators=[DataRequired()])
+    lastname = StringField("LastName", validators=[DataRequired()])
+    grade = StringField("Grade", validators=[DataRequired(), NumberRange(9, 12)])
+    submit = SubmitField('Add Student')
