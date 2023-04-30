b47d17096e0276e5476bf6b116e0c46180a04c48
Alexander-Dorsey <113309630+Alexander-Dorsey@users.noreply.github.com>
2023-04-30 13:17:31 -0500

    Coded UI with other functions now completely works

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import mysql.connector
 connection = mysql.connector.connect(user = 'root',database='bankingproject',password='TheFinisher518!')
 cursor = connection.cursor()
 def check(ID):
+    ID = str(ID)
     Query = ("SELECT accountBalance FROM accounts Where accountID = " + ID + "")
     cursor.execute(Query)
     for item in cursor:
         print(item)
+def info(ID):
+    ID = str(ID)
+    Query = ("SELECT * FROM accounts Where accountID = " + ID + "")
+    cursor.execute(Query)
+    for item in cursor:
+        print(item)
 def create(name,balance,cred,password):
-    balance = float(balance)
-    cred = int(cred)
-    addData = ("INSERT INTO accounts(accountName,accountBalance,credit,password) VALUES('" + name + "'," + balance + "," + cred + "," + password + ")")
+    addData = ("INSERT INTO accounts(accountName,accountBalance,credit,password) VALUES('" + name + "'," + balance + "," + cred + ",'" + password + "')")
     cursor.execute(addData)
     connection.commit()
 def show():
@@ -61,7 +66,7 @@ def modify(ID,data,value):
         change = ("UPDATE accounts SET accountName = \"" + value + "\" WHERE accountID = " + str(ID))
     elif(data[0].upper() == "C"):
         change = ("UPDATE accounts SET credit = " + value + " WHERE accountID = " + str(ID))
-    elif(data[0].upper() == "C"):
+    elif(data[0].upper() == "P"):
         change = ("UPDATE accounts SET password = " + value + " WHERE accountID = " + str(ID))
     else:
         print("Not valid imput")
@@ -69,18 +74,80 @@ def modify(ID,data,value):
     cursor.execute(change)
     connection.commit()
 def login(): 
-    print()
-while(True):
+     n = input("Enter the name for your account\n")
+     n = str(n)
+     p = input("Enter the password of your account\n")
+     p = str(p)
+     Query = ("SELECT * FROM accounts WHERE accountName = '" + n + "' AND password = '" + p + "'")
+     cursor.execute(Query)
+     count = 0
+     for item in cursor:
+         count = count + 1
+         print(item)
+     if(count != 1):
+         print("Account with provided info does not exist please try again")
+         login()
+     for item in cursor:
+        print(item)
+     global identifier
+     q = ("SELECT accountID FROM accounts WHERE password = '" + p + "'")
+     cursor.execute(q)
+     for item in cursor:
+        ident = item
+     identifier = ident[0]
+"""
+cond = True
+while(cond == True):
     act = input("Welcome to the banking app.\nWould you like to create an account or login\n")
-    if(act[0].upper == "L"):
-        if(login()):
-            break
-    elif(act[0].upper == "C"):
-        n = input("Set the name for your account")
-        m = input("Set the starting balance of your account")
-        c = input("Set the credit of your account")
-        p = input("Set the password of your account")
-        create(n,m,c,p)
+    if(act[0].upper() == 'L'):
+            login()
+            cond = False
+    elif(act[0].upper() == 'C'):
+        con = False
+        while(con == False):
+            n = input("Set the name for your account\n")
+            m = input("Set the starting balance of your account\n")
+            c = input("Set the credit of your account\n")
+            p = input("Set the password of your account\n")
+            Query = ("SELECT * FROM accounts Where password = '" + p  + "'")
+            cursor.execute(Query)
+            count = 0
+            for item in cursor:
+                count = count + 1
+            if(count == 0):
+                create(n,m,c,p)
+                con = True
+            else:
+                print("Account with password already exists")
     else:
         print("Invalid Input")
+run = True
+while(run):
+    uIn = input("What would you like to do\nThe Options are:\nCheck Balance\nSee Info\nAdd Money\nWithdraw\nModify account info\nDelete Accout\nQuit\n")
+    if(uIn[0].upper() == 'C'):
+        check(identifier)
+    elif(uIn[0].upper() == 'A'):
+        depo = input("How much would you like to input?\n")
+        add(identifier,depo)
+    elif(uIn[0].upper() == 'W'):
+        loss = input("How much would you like to withdraw\n")
+        withdraw(identifier,loss)
+    elif(uIn[0].upper() == 'M'):
+        change = input("What would you like to change\nYou can change your accounts name password or credit\n")
+        v = input("What would you like to change it to\n")
+        modify(identifier,change,v)
+    elif(uIn[0].upper() == 'D'):
+        confirm = input("Are you sure you would like to delete your account\n")
+        if(confirm[0].upper() == "Y"):
+            delete(identifier)
+            run = False 
+        else:
+            print("Account not deleted")
+    elif(uIn[0].upper() == 'Q'):
+        confirm = input("Are you sure you would like to quit\n")
+        if(confirm[0].upper() == "Y"):
+            run = False
+    elif(uIn[0].upper() == 'S'):
+        info(identifier)
+"""
 connection.close()
